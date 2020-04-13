1be7032841c67fd3903d3773c1fdf1a089109ef0
UdrK <nomefittizioqualunque@gmail.com>
2020-04-13 21:21:53 +0200

    fixing error in prim's algorithm MST weight calculation

--- algorithms.py
+++ algorithms.py
@@ -5,6 +5,7 @@ def prim(g, s):
     MST = []
     MST_weight = 0
     heap = Heap()
+
     for v in g.V:
         if v == s:
             heap.insert([0, v])
@@ -22,9 +23,13 @@ def prim(g, s):
                 u_v_weight = g.weight(u, v) # returns math.inf if no edge is found, however at this point we know there is one
                 if u_v_weight < heap.key(v_position):
                     MST[v-1] = u
-                    MST_weight += u_v_weight
                     heap.delete(v_position)
                     heap.insert([u_v_weight, v])
+
+    for i in range(0, len(MST)):
+        if not MST[i] == 0:
+            MST_weight += g.edges_dict[i+1][MST[i]]
+
     return MST, MST_weight
 
 def naive_kruskal(g):
@@ -39,13 +44,16 @@ def naive_kruskal(g):
     while not done:
         aux_edge = heap.pop()
         edge = [aux_edge[1][0], aux_edge[1][1], aux_edge[0]]
-        if not edge[1] in aux_vertices_dict: # if edge doesn't make it cyclic
-            aux_vertices_dict[edge[1]] = edge[0]
-            MST_weight += edge[2]
-            MST.append(edge)
-        if len(MST) == g.n-1:
-            done = True
+        if not edge[0] == edge[1]:
+            if not(edge[0] in aux_vertices_dict and edge[1] in aux_vertices_dict): # if acyclic (doesn't work)
+                aux_vertices_dict[edge[0]] = 1
+                aux_vertices_dict[edge[1]] = 1
+
+                MST_weight += edge[2]
+                MST.append(edge)
+            if len(MST) == g.n-1 or heap.is_empty():
+                done = True
     return MST, MST_weight
 
-def kruskal(g, s):
+def kruskal(g):
     pass
