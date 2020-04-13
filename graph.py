f97fd6befe8095cf5022713c8316e8373ac2f0a9
UdrK <nomefittizioqualunque@gmail.com>
2020-04-13 21:22:40 +0200

    taking into account edges between same nodes and self-loop edges

--- graph.py
+++ graph.py
@@ -2,35 +2,24 @@ import math
 
 class Graph:
     def __init__(self, V, E):
-        self.V = V
-        self.E = E
-        self.n = len(V)
-        self.m = len(E)
         self.edges_dict = {}
+        self.V = V
+        self.n = len(V)
         for v in V:
             self.edges_dict[v] = {}
 
         for e in E:
-            self.edges_dict[e[0]][e[1]] = e[2]
-
-    @staticmethod
-    def is_acyclic(E):
-        aux_vertices_dict = {}
-
-        for e in E:
-            if e[1] in aux_vertices_dict == False:
-                aux_vertices_dict[e[1]] = e[0]
-            elif aux_vertices_dict[e[1]] != e[0]:
-                return False
-        return True
+            if e[0] != e[1]:
+                if e[0] in self.edges_dict and e[1] in self.edges_dict[e[0]]:
+                    if e[2] < self.edges_dict[e[0]][e[1]]:
+                        self.edges_dict[e[0]][e[1]] = e[2]
+                        self.edges_dict[e[1]][e[0]] = e[2]
+                else:
+                    self.edges_dict[e[0]][e[1]] = e[2]
+                    self.edges_dict[e[1]][e[0]] = e[2]
 
     def adjacents(self, v):
         return self.edges_dict[v].keys()
 
     def weight(self, u, v):
         return self.edges_dict[u][v]
-
-    def print_graph(self):
-        print('{} {}'.format(len(self.V), len(self.E)))
-        for e in self.E:
-            print('{} {} {}'.format(e[0], e[1], e[2]))
