312b2413e0fc51a67838ddc13dd739f625b3a80a
UdrK <nomefittizioqualunque@gmail.com>
2020-04-14 14:29:08 +0200

    main

--- debugging_main.py
+++ debugging_main.py
@@ -14,20 +14,23 @@ weights = []
 
 i = 1
 for file in os.listdir(directory_name):
-     filename = os.fsdecode(file)
-     if filename.endswith(".txt"):
-        file_path = os.path.join(directory_name, filename)
-        file_names.append(file_path)
-        gr = txt_reader.file_to_graph(file_path)
-        beginning = time.time()
+    if i<20:
+        filename = os.fsdecode(file)
+        if filename.endswith(".txt"):
+            file_path = os.path.join(directory_name, filename)
+            file_names.append(file_path)
+            gr = txt_reader.file_to_graph(file_path)
+            beginning = time.time()
 
-        mst, mst_weight = algorithms.prim(gr, 1)
-        weights.append(mst_weight)
-        end = time.time()
-        print("Done... {}, weight: {}".format(i, mst_weight))
-        i += 1
-        times.append(beginning - end)
+            # mst, mst_weight = algorithms.kruskal(gr)
+            mst, mst_weight = algorithms.naive_kruskal(gr)
+            # mst, mst_weight = algorithms.prim(gr, 6)
+            weights.append(mst_weight)
+            end = time.time()
+            print("Done... {}".format(mst_weight))
+            i += 1
+            times.append(end - beginning)
 
-f = open('prim_times.txt', 'w+')
+f = open('naive_kruskal_times.txt', 'w+')
 for t, fn, w in zip(times, file_names, weights):
-    f.write('{}, {}, {}\n'.format(fn, t))
+    f.write('{}, {}, {}\n'.format(fn, t, w))
