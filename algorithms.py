e4f0055d11c87f80e0b60e92e7222c428a4e8dc6
UdrK <nomefittizioqualunque@gmail.com>
2020-04-14 14:28:52 +0200

    kruskal working, naive kruskal almost working

--- algorithms.py
+++ algorithms.py
@@ -1,4 +1,5 @@
 from heap import Heap
+from union_find import UnionFind
 import math
 
 def prim(g, s):
@@ -32,8 +33,40 @@ def prim(g, s):
 
     return MST, MST_weight
 
+def is_acyclic(edges, new_edge):
+
+    # if i've never encountered one of the 2 vertices in the new edge it can't be cyclic
+    if not(new_edge[0] in edges and new_edge[1] in edges):
+        return True
+
+    # otherwise it could be cyclic but maybe not
+    # list of vertices adjacent to the target vertex
+    list_1 = edges[new_edge[1]]
+
+    # list of vertices adjacent to the source vertex, if one of these (or their adjacents) appear in the list of
+    # target's adjacent, there's a loop
+    list_0 = edges[new_edge[0]].copy()
+
+    checked = []
+
+    while len(list_0) > 0:
+        elem = list_0[0]
+        if elem in list_1 or elem == new_edge[1]:
+            return False
+        else:
+            for adj in edges[elem]:
+                if not adj in list_0 and not adj in checked:
+                    list_0.append(adj)
+
+        checked.append(list_0.pop(0))
+
+    return True
+
+
 def naive_kruskal(g):
+
     aux_vertices_dict = {}
+
     MST = []
     MST_weight = 0
     heap = Heap()
@@ -44,16 +77,49 @@ def naive_kruskal(g):
     while not done:
         aux_edge = heap.pop()
         edge = [aux_edge[1][0], aux_edge[1][1], aux_edge[0]]
-        if not edge[0] == edge[1]:
-            if not(edge[0] in aux_vertices_dict and edge[1] in aux_vertices_dict): # if acyclic (doesn't work)
-                aux_vertices_dict[edge[0]] = 1
-                aux_vertices_dict[edge[1]] = 1
 
-                MST_weight += edge[2]
-                MST.append(edge)
-            if len(MST) == g.n-1 or heap.is_empty():
-                done = True
+        if is_acyclic(aux_vertices_dict, edge):
+            if not edge[0] in aux_vertices_dict:
+                aux_vertices_dict[edge[0]] = []
+            if not edge[1] in aux_vertices_dict:
+                aux_vertices_dict[edge[1]] = []
+
+            aux_vertices_dict[edge[0]].append(edge[1])
+            aux_vertices_dict[edge[1]].append(edge[0])
+
+            MST_weight += edge[2]
+            MST.append(edge)
+
+        if len(MST) == g.n-1 or heap.is_empty():
+            done = True
+
     return MST, MST_weight
 
 def kruskal(g):
-    pass
+
+    MST = []
+    MST_weight = 0
+    U = UnionFind()
+    U.initialize(g.V)
+
+    heap = Heap()
+    for e in g.E:
+        heap.insert([e[2], [e[0], e[1]]])
+
+    done = False
+    while not done:
+
+        aux_edge = heap.pop()
+        edge = [aux_edge[1][0], aux_edge[1][1], aux_edge[0]] # original edge format: v1, v2, w
+        e0_find = U.find(edge[0])
+        e1_find = U.find(edge[1])
+
+        if e0_find != e1_find:
+            MST.append(edge)
+            MST_weight += edge[2]
+            U.union(e0_find, e1_find)
+
+        if len(MST) == g.n-1 or heap.is_empty():
+            done = True
+
+    return MST, MST_weight
--- graph.py
+++ graph.py
@@ -3,18 +3,26 @@ import math
 class Graph:
     def __init__(self, V, E):
         self.edges_dict = {}
+        self.E = []
         self.V = V
         self.n = len(V)
         for v in V:
             self.edges_dict[v] = {}
 
+        # formare self.E qua V dentro
         for e in E:
             if e[0] != e[1]:
                 if e[0] in self.edges_dict and e[1] in self.edges_dict[e[0]]:
                     if e[2] < self.edges_dict[e[0]][e[1]]:
                         self.edges_dict[e[0]][e[1]] = e[2]
                         self.edges_dict[e[1]][e[0]] = e[2]
+
+                        for i in range(0, len(self.E)):
+                            if self.E[i][0] == e[0] and self.E[i][1] == e[1]:
+                                self.E[i][2] = e[2]
+                                break
                 else:
+                    self.E.append([e[0], e[1], e[2]])
                     self.edges_dict[e[0]][e[1]] = e[2]
                     self.edges_dict[e[1]][e[0]] = e[2]
 
